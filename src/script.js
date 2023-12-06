59e123e710dfb6dc6648331c9f5a7f9e06778cc6
alices1988 <emy.ally88@gmail.com>
2023-12-06 21:41:16 +0000

    Improved user experience

--- src/script.js
+++ src/script.js
@@ -1,6 +1,7 @@
 function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "response.data.answer",
+    strings: response.data.answer,
     autoStart: true,
     delay: 0.5,
     cursor: "",
@@ -15,7 +16,11 @@ function generatePoem(event) {
   let prompt = `User instructions: Generate a British poem about ${instructionsInput.value}`;
   let context =
     "You are a romantic Poem expert and love to write short poems. Please generate a 4 line poem in basic HTML and separate each line with a <br /> . Please do not include any title. Please make sure to follow the user instructions. At the bottom please sign the poem with `Made by AI` inside a <strong> element.";
-  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key={apiKey}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `Generating a short poem for you about ${instructionsInput.value} in few moments...`;
 
   console.log("Generating poem");
   console.log(`Prompt: ${prompt}`);
