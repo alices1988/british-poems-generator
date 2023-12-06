4aaa54fd56e460a0d4f7acf84662eb6c4aa2faa1
alices1988 <emy.ally88@gmail.com>
2023-12-06 15:06:55 +0000

    Added basic form handling, added typewriter

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-alert("Hello");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "If you can talk with crowds and keep your virtue",
+    autoStart: true,
+    delay: 0.5,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
